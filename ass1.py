b3a282d3501cf9b7edb0545c0ce927e7eef1d6e8
AVISI <kyawthetsoe1011@gmail.com>
2020-11-17 12:13:36 +0630

    simple_calcutor

---
+++ ass1.py
@@ -0,0 +1,9 @@
+header = "SIMPLE CALCULATOR"
+print(header.center(len(header)+10,"*"))
+num1 = int(input("Enter 1st Number: "))
+num2 = int(input("Enter 2nd Number: "))
+print("The addition of your numbers: ", num1 + num2)
+print("The subtraction of your numbers: ", num1 - num2)
+print("The multiplication of your numbers: ", num1 * num2)
+print("The division of your numbers: ", num1 / num2)
+print("The modulus of your numbers: ", num1 % num2)
